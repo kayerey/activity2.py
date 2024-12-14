ee645b10b71e2dc199df8c888242a95f4443708a
Kaye Rey <reykaye6@gmail.com>
2024-12-14 19:52:26 +0800

    Create activity4.py

---
+++ activity4.py
@@ -0,0 +1,7 @@
+number1 = eval(input("Enter a number ---> "))
+
+number2 = eval(input("Enter a another number ---> "))
+
+answer = number1 * number2
+
+print("The sum of" ,number1 ,"and" ,number2 , "is" ,answer)
