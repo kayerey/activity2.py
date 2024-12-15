d309234649186ec84dfb940830423067a6319d8c
Kaye Rey <reykaye6@gmail.com>
2024-12-15 13:22:32 +0800

    Update

---
+++ activity6.py
@@ -0,0 +1,23 @@
+#assignment operator activity 
+
+x = 10
+
+print(x)
+
+#what if, the program requires to add another 10 to the current value of x
+
+a = x + 10 
+
+print(a)
+
+#what if, the program requires to add another 20 to the current value of x
+
+b = x + 20 
+
+print(b)
+
+#what if, the program requires to add another 30 to the current value of x
+
+c = x + 30
+
+print(c)
