f2b69026c30ca5efc09234f6c6476efa57cdb879
Kaye Rey <reykaye6@gmail.com>
2024-12-15 13:28:32 +0800

    Update

---
+++ activity8.py
@@ -0,0 +1,13 @@
+password = input("Enter your password ---> ")
+
+if password.lower() == "secret":
+        print("Access Granted")
+        print("Enjoy using the system")
+ 
+elif password == "gandanya01":
+        print("ako na")
+        print("Enjoy using the system ganda")
+else:
+        print("Access Denied")
+
+print("Thankyou for using system")  
